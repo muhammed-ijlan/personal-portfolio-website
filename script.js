a5f8e1ccb0ff733e69e27842a3b70aedd858d248
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-09 13:46:14 +0530

    modal form added to download cv button with all styles

--- script.js
+++ script.js
@@ -71,4 +71,16 @@ $(document).ready(function () {
     },
   });
 });
-//download functionality
+
+// modal
+const modalBtn = document.querySelector(".modal-btn")
+const modalBg = document.querySelector(".modal-bg")
+const modalClose = document.querySelector(".modal-close")
+
+modalBtn.addEventListener("click", function () {
+  modalBg.classList.add("bg-active")
+})
+
+modalClose.addEventListener("click", function () {
+  modalBg.classList.remove("bg-active")
+})
